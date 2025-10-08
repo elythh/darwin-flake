de0600ec244333f59bc34ba3a93c0d61fff1c61a
elyth <gwen@omg.lol>
2025-10-08 15:21:40 +0200

    :wrench: (aerospace): workspace for obsidian

--- modules/darwin/wms/aerospace/default.nix
+++ modules/darwin/wms/aerospace/default.nix
@@ -112,7 +112,7 @@ in
               app-id = "md.obsidian";
             };
             run = [
-              "move-node-to-workspace 2"
+              "move-node-to-workspace o"
             ];
           }
           {
@@ -173,6 +173,7 @@ in
           alt-2 = "workspace 2";
           alt-3 = "workspace 3";
           alt-4 = "workspace 4";
+          alt-o = "workspace o";
 
           alt-shift-1 = [
             "move-node-to-workspace 1"
