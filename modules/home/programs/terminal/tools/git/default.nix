f3cbbc27daadae9822f18020a01d99239a665cdc
elyth <elyth@gwen-mbp.kooka-eel.ts.net>
2026-02-17 12:44:09 +0100

    :package: (flake): update

--- modules/home/programs/terminal/tools/git/default.nix
+++ modules/home/programs/terminal/tools/git/default.nix
@@ -103,9 +103,9 @@ in
       ];
     };
 
-    sops.secrets.GITPRIVATETOKEN = {
-      path = "${config.home.homeDirectory}/.gitcreds";
-    };
+    # sops.secrets.GITPRIVATETOKEN = {
+    #   path = "${config.home.homeDirectory}/.gitcreds";
+    # };
     sops.secrets.LINODE_TOKEN = {
       path = "${config.home.homeDirectory}/.config/LINODE_TOKEN";
     };
